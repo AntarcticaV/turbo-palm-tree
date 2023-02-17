006acabb5f8b3388a4a87c3df14d3b287fb4d9fc
AntarcticaV <beliyroman@outlook.com>
2023-02-17 18:57:02 +0700

    First commit

---
+++ 21.106_Beliy_4/21.106_Beliy_4/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using _21._106_Beliy_4.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _21._106_Beliy_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSerch_Click(object sender, RoutedEventArgs e)
+        {
+            using ( var db = new Entities())
+            {
+                db.Speciality.Load();
+                List<Group> result =new List<Group>();
+                result = db.Group.Where(o => o.Speciality.Title == "15.02.09 Аддитивные технологии")
+                    .OrderByDescending(g => g.Title).ToList();
+                LoadDate.ItemsSource=result;
+                if (result.Count == 0)
+                {
+                    labelError.Content = "Записи отсутствуют";
+                }
+            }
+        }
+    }
+}
